7218f38f5c1a4a81ac5f91c22282eec5697566bd
Saad <63255607+Heavens-Dark-Carnage@users.noreply.github.com>
2024-11-02 21:19:58 +0400

    Update and rename fetch_students.php to fetch_registrations.php

--- php/fetch_registrations.php
+++ php/fetch_registrations.php
@@ -4,15 +4,9 @@ header('Content-Type: application/json');
 // File path for registrations
 $registrationFile = '../data/registrations.json';
 
-// Check if the file exists
-if (!file_exists($registrationFile)) {
-    echo json_encode([]);
-    exit();
-}
-
-// Read the JSON file
+// Read existing registrations
 $registrations = json_decode(file_get_contents($registrationFile), true);
 
-// Return the registrations as JSON
+// Send the data as JSON
 echo json_encode($registrations);
 ?>
