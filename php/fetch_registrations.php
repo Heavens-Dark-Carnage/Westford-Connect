226a960a9ae6d28d96932ddbdcf5806d2b0f72b4
Saad <63255607+Heavens-Dark-Carnage@users.noreply.github.com>
2024-11-02 21:37:27 +0400

    Update fetch_registrations.php

--- php/fetch_registrations.php
+++ php/fetch_registrations.php
@@ -1,12 +1,8 @@
 <?php
 header('Content-Type: application/json');
 
-// File path for registrations
-$registrationFile = '../data/registrations.json';
-
-// Read existing registrations
-$registrations = json_decode(file_get_contents($registrationFile), true);
-
-// Send the data as JSON
-echo json_encode($registrations);
+// Read registrations data
+$registrationsFile = '../data/registrations.json';
+$registrationsData = file_get_contents($registrationsFile);
+echo $registrationsData;
 ?>
